f7add7c51754320989195f23a56d69c8b92221b0
Ham Seok Won <36294761+holynow@users.noreply.github.com>
2024-02-15 22:15:42 +0900

    [level 0] Title: 배열의 평균값, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.js
@@ -1,11 +1,5 @@
-function solution(numbers) {
-    
-    //[1, 2, 3, 4, 5, 6, 7, 8, 9, 10] 5.5
-    //[89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99] 94.0
-    
-    var answer = 0;
-    numbers.map(v => {
-        answer+=v/numbers.length
-    })
+function solution(numbers) {    
+    let answer = 0;
+    numbers.map(v => answer+=v/numbers.length);
     return answer;
 }
