1294bd9d18fb844eca4fe00c3b5fc05f9c53d465
Ham Seok Won <36294761+holynow@users.noreply.github.com>
2024-02-15 19:35:27 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,6 +1,7 @@
 function solution(my_string, n) {
     var answer = '';
-    let arr = [...my_string];
-    answer = arr.map(v => v.repeat(n)).join('')
+    for (let i = 0; i < my_string.length; i++){
+        console.log(my_string[i].join(""))
+    }
     return answer;
 }
