873a16901c1d93c7a269a795d7c98a179de51dc3
Ham Seok Won <36294761+holynow@users.noreply.github.com>
2024-02-15 22:40:47 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    let count1= 0;
+    let count2 = 0
+    for (let i = 0; i < num_list.length; i++){
+        if(num_list[i] % 2 === 0) {
+            count1++;
+        } else {
+            count2++;
+        }
+    }
+   return [count1, count2]
+}
