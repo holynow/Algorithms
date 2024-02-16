a86a3552b8dd3dc6c0b55853bb6f5eeb7040a792
Ham Seok Won <36294761+holynow@users.noreply.github.com>
2024-02-16 19:20:24 +0900

    [level 1] Title: 시저 암호, Time: 7.97 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.js
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.js
@@ -1,11 +1,12 @@
-function solution(s, n) {
-  var answer = '';
-  for(i = 0; i < s.length; i++) {
-    if(s[i].match(/[a-zA-Z]/)) {
-      const reg = s.charCodeAt(i);
-      const eng = reg <= 90 ? 65 : 97;
-      answer += String.fromCharCode(((reg - eng + n) % 26) + eng);
-    } else {answer += s[i]};
-  } 
-  return answer;
-}
+  const solution = (s, n)=> {
+    let reg, eng, answer = '';
+    let arr = Array.from(s);
+    for(i in arr) {
+        reg = s.charCodeAt(i); 
+        if(s[i].match(/[A-Za-z]/g)) { 
+        eng = reg <= 90 ? 65 : 97; 
+        answer += String.fromCharCode(((reg - eng + n) % 26) + eng); 
+        } else answer += s[i];
+    };
+    return answer;
+  }
