be1a52293de2883aca3c3e1a6fb1f7ffea446873
Ham Seok Won <36294761+holynow@users.noreply.github.com>
2024-02-17 15:36:48 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.js
@@ -1,7 +1,7 @@
-function solution(a, b) {
-    var result = 0;
-    for(let i = Math.min(a,b); i <= Math.max(a,b); i++){
-        result = result + i;
+const solution = (a,b) => {
+    let result = 0;
+    for (let i = Math.min(a,b); i <= Math.max(a,b); i++) {
+        result += i;
     }
     return result;
 }
