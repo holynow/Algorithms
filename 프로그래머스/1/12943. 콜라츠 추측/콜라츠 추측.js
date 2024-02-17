08204bab1a0ba6fe4fb1601ae11e74cd870aca17
Ham Seok Won <36294761+holynow@users.noreply.github.com>
2024-02-17 16:40:48 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,12 +1,8 @@
-function solution(num) {
-let count = 0;
-if (num > 1){
-    while(!(num === 1)){
+const solution = (num) => {
+    let count = 0;
+    while (num !== 1) {
         count++;
-        num = num % 2 === 0 ? num / 2 : num * 3 + 1;
+        num = num%2===0 ? num/2 : num*3+1;
     }
-    return count >= 500 ? -1 : count;
-} else if (num === 1) {
-    return 0;
-    }
-};
+   return count >= 500 ? -1 : count;
+}
