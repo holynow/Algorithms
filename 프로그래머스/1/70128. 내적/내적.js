495ac4ffa64c79f2efd4de587da1643834a2c92d
Ham Seok Won <36294761+holynow@users.noreply.github.com>
2024-03-14 09:43:31 +0900

    [level 1] Title: 내적, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,7 +1,7 @@
-function solution(a, b) {
-  let answer = 0;
-  for(let i = 0; i < a.length; i++) {
-    answer += a[i]*b[i]
-  }
-  return answer;
-}
+const solution = (a,b) => {
+    let result = 0;
+    for (i in b) {
+        result += a[i]*b[i];
+    }
+    return result;
+};
